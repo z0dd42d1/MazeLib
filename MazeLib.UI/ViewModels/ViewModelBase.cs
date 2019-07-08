1e7c76be99c4f90be67847d009d346dbee777491
nexic <>
2019-07-08 20:08:46 +0200

    Added empty Avalonia project.

---
+++ MazeLib.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace MazeLib.UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
