4b58030d68bf1472ee9060c21e7993cee4ac32ed
nexic <MazeLibary@gmail.com>
2019-07-15 22:21:23 +0200

    Cleanup, stylecop, ignored CA1308 because it is not relevant for this project.

--- MazeLib/Drawing/MazeImageCreator.cs
+++ MazeLib/Drawing/MazeImageCreator.cs
@@ -68,7 +68,7 @@ namespace MazeLib
             }
         }
 
-        public static async Task CreateMazeAnimationGifAsync(IList<MazeTransformationStep> mazeTransformationSteps, IMaze maze, string label, int tilesize, string targetPath)
+        public static async Task CreateMazeAnimationGifAsync(IList<MazeTransformationStep> mazeTransformationSteps, IMaze maze, string label, int tilesize)
         {
             if (maze == null) throw new ArgumentNullException(nameof(maze));
 
